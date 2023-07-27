d2777af6e16f7207163fafb5a826ca7abbdea654
s1290005 <s1290005@u-aizu.ac.jp>
2023-07-27 22:31:05 +0900

    完成

---
+++ setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+setuptools.setup(
+    name="s1290005_learn",
+    version="0.0.1",
+    author="s1290005",
+    author_email="s1290005@u-aizu.ac.jp",
+    description="heat map pm25",
+    long_description="ex14",
+    long_description_content_type="text/markdown",
+    url="https://github.com/s1290005/s1290005_learn",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
