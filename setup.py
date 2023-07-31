47631e5ca59c2361663c32b409d1273da95e63e7
s1290006 <s1290006@u-aizu.ac.jp>
2023-07-31 14:59:13 +0900

    first

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 import setuptools
 
 setuptools.setup(
-    name="s1290005_learn",
+    name="s1290006_learn",
     version="0.0.1",
-    author="s1290005",
-    author_email="s1290005@u-aizu.ac.jp",
+    author="s1290006",
+    author_email="s1290006@u-aizu.ac.jp",
     description="heat map pm25",
     long_description="ex14",
     long_description_content_type="text/markdown",
-    url="https://github.com/s1290005/s1290005_learn",
+    url="https://github.com/s1290006/s1290006_learn",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3.7",
